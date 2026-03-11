0d90b77ac34dc232b5a808fd3ca016acf9e25c3f
HideBa <baba.papa1120.ba@gmail.com>
2026-03-11 21:57:28 +0100

    Initial commit

--- src/app/handlers/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod compaction;
-pub mod delete;
-pub mod export;
-pub mod insert;
-pub mod query;
-pub mod table;
-pub mod update;
--- src/core/db/mod.rs
+++
@@ -1,9 +0,0 @@
-mod compaction;
-mod delete;
-mod export;
-mod insert;
-mod metadata;
-mod query;
-pub mod service;
-mod table;
-mod update;
