12787187398b1409f44950d51f4ca10c6b4efb64
HideBa <baba.papa1120.ba@gmail.com>
2026-09-23 22:01:11 +0200

    Revise documentation tutorials and organise examples by tool

--- documents/docs/07-tutorials/(duckdb)/meta.ts
+++ documents/docs/07-tutorials/(duckdb)/meta.ts
@@ -1,6 +1,7 @@
 import { defineMeta } from "blume";
 
 export default defineMeta({
-  title: "Tutorials",
-  icon: "graduation-cap",
+  title: "DuckDB",
+  order: 1,
+  pages: ["duckdb", "duckdb-3d"],
 });
